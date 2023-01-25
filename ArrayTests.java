e382ef0a55023cb5e404b2ffeb90356ff0356fc4
PranavKambhampati <pranav.21950@gmail.com>
2023-01-25 13:41:21 -0800

    Push

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,6 +9,21 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input);
+    System.out.println(input);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input);
+  }
+
+  @Test
+  public void testReversed2(){
+    int[] input = {0,1,2,3};
+    System.out.println(ArrayExamples.reversed(input));
+    assertArrayEquals(new int[]{3,2,1,0}, ArrayExamples.reversed(input));
+  }
+
 
   @Test
   public void testReversed() {
